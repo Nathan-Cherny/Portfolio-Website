738006a27df89487a77648d048038cf51a055328
Natann826 <natec3632@gmail.com>
2024-01-04 11:54:25 -0500

    home updates, navbar redo, smaller things

--- static/js/main.js
+++ static/js/main.js
@@ -29,5 +29,53 @@ async function showJsonData(json){ // json can be either skills or projects
     }
 }
 
-showJsonData("skills")
-showJsonData("learning")
+function whenLoaded(){
+    getSectionsBoundaries()
+    showJsonData("skills")
+    showJsonData("learning")
+    document.onscroll = checkSectionActivity
+}
+
+function getSectionsBoundaries(){
+    boundaries = []
+
+    let sections = document.getElementsByTagName("section")
+    let i = 0
+    while(i < sections.length - 1){
+        let section = sections[i]
+        let top = sections[i].offsetTop
+        let bottom = top + sections[i].offsetHeight
+        boundaries.push({
+            "section": section,
+            "top": top,
+            "bottom": bottom
+        })
+        i++
+    }
+}
+
+function changeActivity(active, li){
+    active.classList.remove("active")
+    li.classList.add("active")
+}
+
+function checkSectionActivity(){
+    let currentScroll = document.documentElement.scrollTop
+    let lis = document.getElementsByTagName("li")
+    let activeLi = document.getElementsByClassName("active")[0]
+
+    let i = 0
+    while(i < boundaries.length){
+        let sectionData = boundaries[i]    
+        let section = sectionData["section"]
+        let li = lis[i]
+
+        let liHrefToId = li.children[0].hash.slice(1)
+
+        if (sectionData["top"] <= currentScroll && currentScroll <= sectionData["bottom"] && activeLi != li){
+            changeActivity(activeLi, li)
+        }
+
+        i++
+    }
+}
